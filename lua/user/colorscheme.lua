9cfdefd5242e4fbef26f7de69211240c6bea1995
xx <xx@outlook.com>
2022-10-30 11:11:49 +0800

    init nvim2

---
+++ lua/user/colorscheme.lua
@@ -0,0 +1,7 @@
+-- local colorscheme = "tokyonight-night"
+local colorscheme = "gruvbox-material"
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  return
+end

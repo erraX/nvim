0a3b32586c5c9da7438a0b46c1c038322ee6cad5
Minjie Ni <minjie.ni@booking.com>
2022-10-31 19:23:42 +0800

    soft gruvbox

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,6 +1,8 @@
 -- local colorscheme = "tokyonight-night"
 local colorscheme = "gruvbox-material"
 
+vim.g.gruvbox_material_background = "soft"
+
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   return

0606d3f5631037401cf3681252e843164832b991
Minjie Ni <minjie.ni@booking.com>
2022-10-31 19:27:39 +0800

    use volar as vue lsp

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -3,8 +3,10 @@ if not status_ok then
   return
 end
 
+vim.lsp.set_log_level("trace")
+
 require "user.lsp.mason"
 require("user.lsp.handlers").setup()
 require "user.lsp.null-ls"
--- require "user.lsp.volar"
-require "user.lsp.vuels"
+require "user.lsp.volar"
+-- require "user.lsp.vuels"

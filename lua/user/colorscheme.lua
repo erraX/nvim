aaeba771138113618c8d2a3acd6266bc9b9cdee6
Minjie Ni <minjie.ni@booking.com>
2022-11-01 22:00:09 +0800

    update gruvbox colorscheme variant

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -2,6 +2,7 @@
 local colorscheme = "gruvbox-material"
 
 vim.g.gruvbox_material_background = "soft"
+vim.g.gruvbox_material_foreground = "original"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then

7c21ee8f45382faec38ccdb8e5d027b568866676
erraX <niminjiecide@outlook.com>
2025-08-29 18:49:18 +0800

    update

--- init.lua
+++ init.lua
@@ -1015,7 +1015,8 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'tokyonight-storm'
     end,
   },
 

633ae2284363bb4a7f8a642230858f949a5f34fb
erraX <niminjiecide@outlook.com>
2025-09-01 20:03:02 +0800

    termtoggle

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -1,5 +1,11 @@
 return {
   'akinsho/toggleterm.nvim',
   version = '*',
-  config = true,
+  opts = {
+    open_mapping = [[<c-\>]],
+    direction = 'float',
+    float_opts = {
+      border = 'curved',
+    },
+  },
 }

01f07cd47f542e9bebbea647501541f78d3001b2
Minjie Ni <minjie.ni@booking.com>
2025-09-05 16:24:49 +0800

    add snippets

--- init.lua
+++ init.lua
@@ -935,12 +935,15 @@ require('lazy').setup({
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              -- require('luasnip.loaders.from_vscode').lazy_load()
+              require('luasnip.loaders.from_vscode').lazy_load {
+                paths = { vim.fn.expand '~/.config/nvim/my-snippets' },
+              }
+            end,
+          },
         },
         opts = {},
       },

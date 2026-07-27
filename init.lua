d94f6a13f2e3920d4413a0f307fef2b69713fe4a
Minjie Ni <minjie.ni@booking.com>
2026-07-27 11:01:55 +0800

    update nvim

--- init.lua
+++ init.lua
@@ -881,6 +881,7 @@ require('lazy').setup({
       vim.lsp.enable { 'vtsls', 'vue_ls' }
       vim.lsp.enable 'gopls'
       vim.lsp.enable 'perlnavigator'
+      vim.lsp.enable 'pyright'
 
       -- ESLint is configured above in `servers` and enabled via mason-lspconfig handler
 
@@ -1136,11 +1137,11 @@ require('lazy').setup({
   },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
+    lazy = false, -- main branch does not support lazy-loading
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    opts = {
-      ensure_installed = {
+    config = function()
+      local ensure_installed = {
         'bash',
         'javascript',
         'typescript',
@@ -1159,25 +1160,36 @@ require('lazy').setup({
         'go',
         'vim',
         'vimdoc',
-      },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        disable = { 'dockerfile' },
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+        'swift',
+        'python',
+      }
+
+      require('nvim-treesitter').install(ensure_installed)
+
+      -- Highlight: filetypes where a parser exists, except dockerfile (legacy regex syntax).
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function(args)
+          local ft = args.match
+          if ft == 'dockerfile' then
+            return
+          end
+          pcall(vim.treesitter.start)
+        end,
+      })
+
+      -- Indent (experimental): skip ruby per the previous setup.
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function(args)
+          local ft = args.match
+          if ft == 'ruby' then
+            return
+          end
+          if vim.treesitter.language.get_lang(ft) then
+            vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
+      })
+    end,
   },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
